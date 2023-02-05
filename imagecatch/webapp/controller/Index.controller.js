80bc9af4862fb59ae8705358d266c6fa73bc0a5f
EdinhoNasciment <edson.nascimento@aluno.ifsp.edu.br>
2023-02-05 18:27:17 -0300

    Terceira Aula finalizada

--- imagecatch/webapp/controller/Index.controller.js
+++ imagecatch/webapp/controller/Index.controller.js
@@ -11,25 +11,7 @@ sap.ui.define([
         return Controller.extend("imagecatch.controller.Index", {
             onInit: function () {
                 var imageList = {
-                    Imagens : [
-                    {
-                        url : "https://cdn.albumoftheyear.org/album/78827-coisas.jpg",
-                        thumbnail : "https://rapidapi.usearch.com/api/thumbnail/get?value=2725385943732621789",
-                        title : "Moacir Santos - Coisas - Reviews - Album of The Year",
-                        provider : {
-                            name : "albumoftheyear"
-                        }
-                    },
-                    {    
-
-                        url : "https://cdn.albumoftheyear.org/album/78827-coisas.jpg",
-                        thumbnail : "https://rapidapi.usearch.com/api/thumbnail/get?value=2725385943732621789",
-                        title : "Moacir Santos - Coisas - Reviews - Album of The Year",
-                        provider : {
-                            name : "albumoftheyear"
-                        }
-                    }
-                    ]
+                    Imagens : []
                 };
 
                 let ImageModel = new JSONModel(imageList);
@@ -43,7 +25,40 @@ sap.ui.define([
             
                 let inputBusca = this.byId("ip_busca");
                 let query = inputBusca.getValue();
-                alert(query);
+                //alert(query);
+
+                const settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q="+ query +"&pageNumber=1&pageSize=10&autoCorrect=true",
+                    "method": "GET",
+                    "headers": {
+                        "X-RapidAPI-Key": "2ba6856604mshfdcdb090da42fd7p1c1152jsnc7a4b11453e3",
+                        "X-RapidAPI-Host": "contextualwebsearch-websearch-v1.p.rapidapi.com"
+                    }
+                };
+                
+                $.ajax(settings).done(function (response) {
+                    console.log(response);
+                
+                    let oImageModel = this.getView().getModel("ModeloImagem");
+                    let oDadosImage = oImageModel.getData();
+                
+                    //Limpando o array
+                    oDadosImage.Imagens = [];
+
+
+                    let listaResultados = response.value ;
+                    let newItem;
+
+                    for(let i = 0; i < listaResultados.length; i++){
+
+                        newItem = listaResultados[i];
+                        oDadosImage.Imagens.push(newItem);
+                    }
+
+                    oImageModel.refresh();
+                }.bind(this));
             
             }
         });
